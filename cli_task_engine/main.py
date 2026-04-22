d00587c3b71a5066edaea75cd3f0fad0b113086a
JermuH92 <jtkukkohovi@hotmail.com>
2026-04-22 17:47:35 +0300

    Build task completion tool

--- cli_task_engine/main.py
+++ cli_task_engine/main.py
@@ -12,17 +12,32 @@ def create_store():
             "name": task_name,
             "completed": False
         }
-
         tasks.append(new_task_dict)
+
+
+    def complete_task(task_id):
+        for task in tasks:
+            if task["id"] == task_id:
+                task["completed"] = True
+
+
     return {
+
         "get": get_tasks,
-        "add": add_task
+        "add": add_task,
+        "complete": complete_task
+
     }
-    
+
+
     
 
 my_store = create_store()
 my_store["add"]("Buy soda")
 my_store["add"]("Code a database")
+my_store["add"]("Practice closures")
+
+my_store["complete"](1)
+my_store["complete"](2)
 
 print(my_store["get"]())
